204b42ac956924aef5b210d4d98ea131e19b0191
Chedulla Sainatha Reddy <161631451+ChedullaSainathaReddy@users.noreply.github.com>
2025-07-30 17:21:36 +0530

    Delete Day3_assignment-master/Day3_assignment-master/src directory

--- Day3_assignment-master/Day3_assignment-master/src/Bank/BankOperations.java
+++
@@ -1,9 +0,0 @@
-package Bank;
-
-public interface BankOperations {
-    void deposit(double amount);
-    void withdraw(double amount);
-    void transfer(Account target, double amount);
-    double checkBalance();
-    void showTransactionHistory();
-}
